5b120bf85257b4c938f38a48ec1be462da744412
luisgchow <luis_gonzalez@trendmicro.com>
2019-07-22 16:33:34 -0500

    fix compose

---
+++ code/index.php
@@ -0,0 +1,10 @@
+hola mundo docker-compose
+
+<?php
+$enlace = mysql_connect('mysql', 'root', 'TM1q2w3e.');
+if (!$enlace) {
+	die('No pudo conectarse: ' . mysql_error());
+}
+echo 'Conectado satisfactoriamente';
+mysql_close($enlace);
+?>
